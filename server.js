fe75e2dcf7ad66796ba3c0faf585b1d1b70d890e
devastro-ap <devendrakushwahaclass10d62@gmail.com>
2025-05-22 18:58:23 +0530

    server.js

--- server.js
+++ server.js
@@ -3,20 +3,20 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
+// Serve static files from 'public' folder
 app.use(express.static('public'));
 
+// Socket.io logic
 io.on('connection', (socket) => {
   console.log('A user connected');
-
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
   });
-
   socket.on('disconnect', () => {
     console.log('A user disconnected');
   });
 });
 
-http.listen(3000, () => {
-  console.log('Server listening on http://localhost:3000');
+http.listen(process.env.PORT || 3000, () => {
+  console.log('Server running');
 });
